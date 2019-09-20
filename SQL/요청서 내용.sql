62caa89bc4a1c39693d07c0ae1e100d20e56b1ad
son9zuen <51072273+son9zuen@users.noreply.github.com>
2019-09-20 16:55:26 +0900

    190920 sql최종
    고수 상세페이지, 고수 리스트, 요청서 리스트, 요청서 상세 페이지

--- SQL/요청서 내용.sql
+++ SQL/요청서 내용.sql
@@ -1,8 +1,7 @@
-
-SELECT r.request_idx, quest_name, answer_cont,m_idx, m_name, m_photo,cate_name, service_name FROM toti.toti_answer a 
+SELECT r.request_idx, quest_name, answer_cont, m.m_idx, m_name, m_photo,cate_name, service_name FROM toti.toti_answer a 
 join toti.toti_question q on q.quest_idx = a.quest_idx 
 join toti.toti_request r on r.request_idx = a.request_idx join toti.toti_member m on m.m_idx = r.m_idx 
 join toti.toti_cate c on q.cate_idx = c.cate_idx
 left outer join (select cate_idx, service_name, m_idx from toti.toti_service se 
 join toti.toti_mentee_service tee on se.service_idx = tee.service_idx ) cate on cate.cate_idx = c.cate_idx
-where r.request_idx =2
+where r.request_idx =1
--- SQL/요청서 리스트.sql
+++ SQL/요청서 리스트.sql
@@ -1,5 +1,8 @@
-SELECT a.request_idx,m.m_idx, m_name, m_photo, c.cate_name, quest_name, answer_cont, service_name, request_date FROM toti.toti_answer a left outer join toti.toti_question q on q.quest_idx = a.quest_idx 
-join toti.toti_request r on r.request_idx = a.request_idx join toti.toti_member m on m.m_idx = r.m_idx 
+SELECT a.request_idx,m.m_idx, m_name, m_photo, c.cate_name, quest_name, answer_cont, service_name, request_date 
+FROM toti.toti_answer a 
+left outer join toti.toti_question q on q.quest_idx = a.quest_idx 
+join toti.toti_request r on r.request_idx = a.request_idx
+join toti.toti_member m on m.m_idx = r.m_idx 
 join toti.toti_cate c on q.cate_idx = c.cate_idx
 join toti.toti_mentee_service tee on m.m_idx =  tee.m_idx
 join toti.toti_service s on s.service_idx = tee.service_idx and c.cate_idx = s.cate_idx
